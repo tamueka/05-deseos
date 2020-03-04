13bd1b51baf171008a5fd465b38616b1b9bdf630
Samuel Ruiz <samuel.ruiz@aktios.com>
2020-03-04 15:17:39 +0100

    añadimos animate.css para la anaimacion al añadir item a ala lista + añadimos ngmodel= item.completado para verificar que esta marcado  + ionchange

--- src/app/pages/agregar/agregar.page.ts
+++ src/app/pages/agregar/agregar.page.ts
@@ -41,4 +41,24 @@ export class AgregarPage implements OnInit {
 
   }
 
+  cambioCheck(item: ListaItem){
+
+    const pendientes = this.lista.items
+                                  .filter(itemData => !itemData.completado)
+                                  .length
+
+    if (pendientes === 0){
+      this.lista.terminadaEn = new Date();
+      this.lista.terminada = true;
+    } else {
+      this.lista.terminadaEn = null;
+      this.lista.terminada = false;
+    }
+
+    this.deseosService.guardarStorage();
+
+    console.log(this.deseosService.listas)
+
+  }
+
 }
